206dc7f1804951b3a5eebe058d38558aa001bb16
AlessioNatale <alessionatale12@gmail.com>
2023-07-13 12:47:00 +0200

    nuovi

--- team.js
+++ team.js
@@ -1,52 +1,14 @@
 const team = [ {
-<<<<<<< HEAD
-
-    name: "Curtis",
-    surname: "Ducasse",
-    age: 24,
-    city: "Palermo",
-    hobby: "Videogiochi",
-    favoriteFood: "pasta",
-    favoriteVideoGame: "League of Legends",
-    favoriteFilm: "Memento",
-    favoriteBook: "L'arte di amare",
-    petName: "Coco",
-} ,
-
-    name: "Sviatoslav",
-    surname: "Larionov",
-    age: 19,
-    city: "Biella",
-    hobby: "Skate",
-    favoriteFood: "Rice",
-    favoriteVideoGame: "CS:GO",
-    favoriteFilm: "Batman",
-    favoriteBook: "Demian",
-    petName: "",
-    },
-    {
-    name = "Vincenzo",
-    surname = "Federico",
-    age = 28,
-    city = "Palermo",
-    hobby = "calcio",
-    favoriteFood = "pasta",
-    favoriteVideoGame = "Diablo",
-    favoriteFilm = "Limitless",
-    favoriteBook = "HarryPotter",
-    petName = "Argo",
-=======
-    name = "Alessio"
-    surname = "Natale"
-    age = 23
-    city = "Avellino"
+    name = "Vincenzo"
+    surname = "Federico"
+    age = 28
+    city = "Palermo"
     hobby = "calcio"
     favoriteFood = "pasta"
-    favoriteVideoGame = "Minecraft"
-    favoriteFilm = "prova a prendermi"
+    favoriteVideoGame = "Diablo"
+    favoriteFilm = "Limitless"
     favoriteBook = "HarryPotter"
-    petName = "pippo"
->>>>>>> alessio
+    petName = "Argo"
 } ];
 
 
