be55874df9953ff8baf8b0b702d185d0a9d3d67a
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-07-13 17:55:27 +0200

    Modificato

--- team.js
+++ team.js
@@ -1,14 +1,14 @@
 const team = [ {
-    name = "Vincenzo"
-    surname = "Federico"
-    age = 28
-    city = "Palermo"
-    hobby = "calcio"
-    favoriteFood = "pasta"
-    favoriteVideoGame = "Diablo"
-    favoriteFilm = "Limitless"
-    favoriteBook = "HarryPotter"
-    petName = "Argo"
+    name: "Vincenzo"
+    surname: "Federico"
+    age: 28
+    city: "Palermo"
+    hobby: "calcio"
+    favoriteFood: "pasta"
+    favoriteVideoGame: "Diablo"
+    favoriteFilm: "Limitless"
+    favoriteBook: "HarryPotter"
+    petName: "Argo"
 } ];
 
 
