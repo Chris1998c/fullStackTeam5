db4f265c1b65aa40f2c1fb980a8ebb8e158fae84
AlessioNatale <alessionatale12@gmail.com>
2023-07-13 19:09:40 +0200

    Fixed

--- team.js
+++ team.js
@@ -1,14 +1,14 @@
 const team = [ {
-    name = "Alessio"
-    surname = "Natale"
-    age = 23
-    city = "Avellino"
-    hobby = "calcio"
-    favoriteFood = "pasta"
-    favoriteVideoGame = "Minecraft"
-    favoriteFilm = "prova a prendermi"
-    favoriteBook = "HarryPotter"
-    petName = "pippo"
+    name : "Alessio",
+    surname : "Natale",
+    age : 23,
+    city : "Avellino",
+    hobby : "calcio",
+    favoriteFood : "pasta",
+    favoriteVideoGame : "Minecraft",
+    favoriteFilm : "prova a prendermi",
+    favoriteBook : "HarryPotter",
+    petName : "pippo"
 } ];
 
 
