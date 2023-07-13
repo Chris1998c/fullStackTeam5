3509d5a9cb3a4a8657399466ebd3eceb723beee6
Sven <chanck.mds@gmail.com>
2023-07-13 11:48:44 +0200

    Object is done

--- team.js
+++ team.js
@@ -1,1 +1,12 @@
-const team = [];
+const team = [ {
+    name: "Sviatoslav",
+    surname: "Larionov",
+    age: 19,
+    city: "Biella",
+    hobby: "Skate",
+    favoriteFood: "Rice",
+    favoriteVideoGame: "CS:GO",
+    favoriteFilm: "Batman",
+    favoriteBook: "Demian",
+    petName: "",
+} ];
