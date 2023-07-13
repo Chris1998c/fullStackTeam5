2529d1b1862d213ed369e51b15fdf785683586de
Chris1998c <contechristian.98@gmail.com>
2023-07-13 11:53:40 +0200

    aggiunti file christian

--- team.js
+++ team.js
@@ -1,1 +1,12 @@
-const team = [];
+const team = [{
+    name: "Christian",
+    surname:"Conte",
+    age:"24",
+    city:"corigliano-rossano",
+    hobby:"hobby",
+    favoriteFood:"Carbonara",
+    favoriteVideoGame: "call of duty",
+    favoriteFilm: "HarryPotter",
+    favoriteBook: "HarryPotter",
+    petName: "",
+}];
