532756d2202403ecc79e8d48960a860844c56732
BlueBojack <curtis.ducasse1g@gmail.com>
2023-07-13 11:54:02 +0200

    Added array

--- team.js
+++ team.js
@@ -1,1 +1,12 @@
-const team = [];
+const team = [ {
+    name: "Curtis",
+    surname: "Ducasse",
+    age: 24,
+    city: "Palermo",
+    hobby: "Videogiochi",
+    favoriteFood: "pasta",
+    favoriteVideoGame: "League of Legends",
+    favoriteFilm: "Memento",
+    favoriteBook: "L'arte di amare",
+    petName: "Coco",
+} ];
