d7a0415c5d0fe15d2a00235d5a36e79aa234958d
Chris1998c <contechristian.98@gmail.com>
2023-07-14 11:49:03 +0200

    nuovi dati christian

--- team.js
+++ team.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const team = [{
     name: "Christian",
     surname:"Conte",
@@ -9,5 +10,30 @@ const team = [{
     favoriteFilm: "HarryPotter",
     favoriteBook: "HarryPotter",
     petName: "",
-}];
+}
 
+const team =  {
+    name: "Sviatoslav",
+    surname: "Larionov",
+    age: 19,
+    city: "Biella",
+    hobby: "Skate",
+    favoriteFood: "Rice",
+    favoriteVideoGame: "CS:GO",
+    favoriteFilm: "Batman",
+    favoriteBook: "Demian",
+    petName: "",
+    },
+    {
+    name: "Vincenzo",
+    surname: "Federico",
+    age: 28,
+    city: "Palermo",
+    hobby: "calcio",
+    favoriteFood: "pasta",
+    favoriteVideoGame: "Diablo",
+    favoriteFilm: "Limitless",
+    favoriteBook: "HarryPotter",
+    petName: "Argo",
+}];
+>>>>>>> bf936b506f9e231f2e28fe77139e681488a411c6
