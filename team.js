ebbe504dcf0bf1dcf9db4a91952e90020183638e
Sven <chanck.mds@gmail.com>
2023-07-14 12:05:04 +0200

    restored

--- team.js
+++ team.js
@@ -1,1 +1,39 @@
-const team = [];
+const team = [ {
+
+    name: "Curtis",
+    surname: "Ducasse",
+    age: 24,
+    city: "Palermo",
+    hobby: "Videogiochi",
+    favoriteFood: "pasta",
+    favoriteVideoGame: "League of Legends",
+    favoriteFilm: "Memento",
+    favoriteBook: "L'arte di amare",
+    petName: "Coco",
+} ,
+{
+    name: "Sviatoslav",
+    surname: "Larionov",
+    age: 19,
+    city: "Biella",
+    hobby: "Skate",
+    favoriteFood: "Rice",
+    favoriteVideoGame: "CS:GO",
+    favoriteFilm: "Batman",
+    favoriteBook: "Demian",
+    petName: "",
+    },
+    {
+    name: "Vincenzo",
+    surname: "Federico",
+    age: 28,
+    city: "Palermo",
+    hobby: "calcio",
+    favoriteFood: "pasta",
+    favoriteVideoGame: "Diablo",
+    favoriteFilm: "Limitless",
+    favoriteBook: "HarryPotter",
+    petName: "Argo",
+} ];
+
+
