672d75f68f1549db4001b5de40bab921b769b77e
BlueBojack <curtis.ducasse1g@gmail.com>
2023-07-18 10:07:15 +0200

    Added League function

--- team.js
+++ team.js
@@ -10,3 +10,10 @@ const team = [ {
     favoriteBook: "L'arte di amare",
     petName: "Coco",
 } ];
+
+function printLeaguePlayer () {
+    for ( i=0; i<team.length;i++)
+    {team[i].favoriteVideoGame=="League of Legends" 
+    ? console.log(team[i].name+" plays "+team[i].favoriteVideoGame):console.log()
+    }}
+    printLeaguePlayer()
