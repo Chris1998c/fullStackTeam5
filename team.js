0aba694f022321a34c0a6648e303aeba5ed460bf
VincenzoFederico95 <138507676+VincenzoFederico95@users.noreply.github.com>
2023-07-18 10:53:23 +0200

    Update team.js
    Puff

--- team.js
+++ team.js
@@ -1,15 +1,3 @@
-const team = [ {
-    name = "Vincenzo"
-    surname = "Federico"
-    age = 28
-    city = "Palermo"
-    hobby = "calcio"
-    favoriteFood = "pasta"
-    favoriteVideoGame = "Diablo"
-    favoriteFilm = "Limitless"
-    favoriteBook = "HarryPotter"
-    petName = "Argo"
-} ];
 
 
 
